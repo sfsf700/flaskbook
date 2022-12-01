f8c4633cec60fb515387cc872e1bd06ef0ce768f
sfsf700 <hirokazu.2rnk@gmail.com>
2022-12-01 11:18:31 +0900

    画像アップロード画面テスト

--- tests/detector/test_views.py
+++ tests/detector/test_views.py
@@ -1,4 +1,11 @@
-# 未ログイン時は 
+from pathlib import Path
+
+from flask.helpers import get_root_path
+from werkzeug.datastructures import FileStorage
+from apps.detector.models import UserImage
+
+# 画像一覧画面のテスト
+# 未ログイン時
 def test_index(client):
 
     rv = client.get("/")
@@ -22,3 +29,52 @@ def test_index_signup(client):
     # ログアウトと画像新規登録ボタンがあるかテスト
     assert "ログアウト" in rv.data.decode()
     assert "画像新規登録" in rv.data.decode()
+
+# 画像アップロード画面のテスト
+# 未ログイン時
+def test_upload_no_auth(client):
+    rv = client.get("/upload", follow_redirects=True)
+
+    assert "アップロード" not in rv.data.decode()
+
+    assert "メールアドレス" in rv.data.decode()
+    assert "パスワード" in rv.data.decode()
+
+# ログイン時
+def test_upload_signup_get(client):
+    signup(client, "admin", "flaskbook@example.com", "password")
+    rv = client.get("/upload")
+
+    assert "アップロード" in rv.data.decode()
+
+# バリデートエラー
+def upload_image(client, image_path):
+    """画像アップロード"""
+    image = Path(get_root_path("tests"), image_path)
+
+    test_file = (
+        FileStorage(
+            stream=open(image, "rb"),
+            filename=Path(image_path).name,
+            content_type="multipart_form-data",
+        ),
+    )
+
+    data = dict(
+        image=test_file,
+    )
+    return client.post("/upload", data=data, follow_redirects=True)
+
+def test_upload_signup_post_validate(client):
+    signup(client, "admin", "flaskbook@example.com", "password")
+    rv = upload_image(client, "detector/testdata/test_invalid_file.txt")
+    # ”サポートされていない画像形式です。" apps/detector/forms.py で指定したエラーメッセージ
+    assert "サポートされていない画像形式です。" in rv.data.decode()
+
+# 画像アップロード成功時
+def test_upload_signup_post(client):
+    signup(client, "admin", "flaskbook@example.com", "password")
+    rv = upload_image(client, "detector/testdata/dog.jpg")
+
+    user_image = UserImage.query.first()
+    assert user_image.image_path in rv.data.decode()
